8c27a24dca3680026c41dd77ef83b09669c3fb2b
MJBoling <35741994+MJBoling@users.noreply.github.com>
2018-11-08 15:32:32 -0500

    Add files via upload

---
+++ Program 3/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Program_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.timeMessageLabel = new System.Windows.Forms.Label();
+            this.timeOutputLabel = new System.Windows.Forms.Label();
+            this.dateOutputLabel = new System.Windows.Forms.Label();
+            this.dateMessageLabel = new System.Windows.Forms.Label();
+            this.showDateButton = new System.Windows.Forms.Button();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.classLabel = new System.Windows.Forms.Label();
+            this.freshmanButton = new System.Windows.Forms.RadioButton();
+            this.sophomoreButton = new System.Windows.Forms.RadioButton();
+            this.juniorButton = new System.Windows.Forms.RadioButton();
+            this.seniorButton = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // timeMessageLabel
+            // 
+            this.timeMessageLabel.AutoSize = true;
+            this.timeMessageLabel.Location = new System.Drawing.Point(14, 163);
+            this.timeMessageLabel.Name = "timeMessageLabel";
+            this.timeMessageLabel.Size = new System.Drawing.Size(176, 13);
+            this.timeMessageLabel.TabIndex = 23;
+            this.timeMessageLabel.Text = "The earliest time you may register is:";
+            // 
+            // timeOutputLabel
+            // 
+            this.timeOutputLabel.AutoSize = true;
+            this.timeOutputLabel.Location = new System.Drawing.Point(208, 163);
+            this.timeOutputLabel.Name = "timeOutputLabel";
+            this.timeOutputLabel.Size = new System.Drawing.Size(0, 13);
+            this.timeOutputLabel.TabIndex = 22;
+            // 
+            // dateOutputLabel
+            // 
+            this.dateOutputLabel.AutoSize = true;
+            this.dateOutputLabel.Location = new System.Drawing.Point(208, 132);
+            this.dateOutputLabel.Name = "dateOutputLabel";
+            this.dateOutputLabel.Size = new System.Drawing.Size(0, 13);
+            this.dateOutputLabel.TabIndex = 21;
+            // 
+            // dateMessageLabel
+            // 
+            this.dateMessageLabel.AutoSize = true;
+            this.dateMessageLabel.Location = new System.Drawing.Point(12, 132);
+            this.dateMessageLabel.Name = "dateMessageLabel";
+            this.dateMessageLabel.Size = new System.Drawing.Size(178, 13);
+            this.dateMessageLabel.TabIndex = 20;
+            this.dateMessageLabel.Text = "The earliest date you may register is:";
+            // 
+            // showDateButton
+            // 
+            this.showDateButton.Location = new System.Drawing.Point(244, 71);
+            this.showDateButton.Name = "showDateButton";
+            this.showDateButton.Size = new System.Drawing.Size(75, 23);
+            this.showDateButton.TabIndex = 19;
+            this.showDateButton.Text = "Show Date";
+            this.showDateButton.UseVisualStyleBackColor = true;
+            this.showDateButton.Click += new System.EventHandler(this.showDateButton_Click);
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(211, 25);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(140, 20);
+            this.lastNameTextBox.TabIndex = 18;
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Location = new System.Drawing.Point(208, 9);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(120, 13);
+            this.lastNameLabel.TabIndex = 17;
+            this.lastNameLabel.Text = "What is your last name?";
+            // 
+            // classLabel
+            // 
+            this.classLabel.AutoSize = true;
+            this.classLabel.Location = new System.Drawing.Point(12, 9);
+            this.classLabel.Name = "classLabel";
+            this.classLabel.Size = new System.Drawing.Size(158, 13);
+            this.classLabel.TabIndex = 12;
+            this.classLabel.Text = "What class are you currently in?";
+            // 
+            // freshmanButton
+            // 
+            this.freshmanButton.AutoSize = true;
+            this.freshmanButton.Location = new System.Drawing.Point(25, 94);
+            this.freshmanButton.Name = "freshmanButton";
+            this.freshmanButton.Size = new System.Drawing.Size(71, 17);
+            this.freshmanButton.TabIndex = 16;
+            this.freshmanButton.TabStop = true;
+            this.freshmanButton.Text = "Freshman";
+            this.freshmanButton.UseVisualStyleBackColor = true;
+            // 
+            // sophomoreButton
+            // 
+            this.sophomoreButton.AutoSize = true;
+            this.sophomoreButton.Location = new System.Drawing.Point(25, 71);
+            this.sophomoreButton.Name = "sophomoreButton";
+            this.sophomoreButton.Size = new System.Drawing.Size(79, 17);
+            this.sophomoreButton.TabIndex = 15;
+            this.sophomoreButton.TabStop = true;
+            this.sophomoreButton.Text = "Sophomore";
+            this.sophomoreButton.UseVisualStyleBackColor = true;
+            // 
+            // juniorButton
+            // 
+            this.juniorButton.AutoSize = true;
+            this.juniorButton.Location = new System.Drawing.Point(25, 48);
+            this.juniorButton.Name = "juniorButton";
+            this.juniorButton.Size = new System.Drawing.Size(53, 17);
+            this.juniorButton.TabIndex = 14;
+            this.juniorButton.TabStop = true;
+            this.juniorButton.Text = "Junior";
+            this.juniorButton.UseVisualStyleBackColor = true;
+            // 
+            // seniorButton
+            // 
+            this.seniorButton.AutoSize = true;
+            this.seniorButton.Location = new System.Drawing.Point(25, 25);
+            this.seniorButton.Name = "seniorButton";
+            this.seniorButton.Size = new System.Drawing.Size(55, 17);
+            this.seniorButton.TabIndex = 13;
+            this.seniorButton.TabStop = true;
+            this.seniorButton.Text = "Senior";
+            this.seniorButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 191);
+            this.Controls.Add(this.timeMessageLabel);
+            this.Controls.Add(this.timeOutputLabel);
+            this.Controls.Add(this.dateOutputLabel);
+            this.Controls.Add(this.dateMessageLabel);
+            this.Controls.Add(this.showDateButton);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.classLabel);
+            this.Controls.Add(this.freshmanButton);
+            this.Controls.Add(this.sophomoreButton);
+            this.Controls.Add(this.juniorButton);
+            this.Controls.Add(this.seniorButton);
+            this.Name = "Form1";
+            this.Text = "Program 3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label timeMessageLabel;
+        private System.Windows.Forms.Label timeOutputLabel;
+        private System.Windows.Forms.Label dateOutputLabel;
+        private System.Windows.Forms.Label dateMessageLabel;
+        private System.Windows.Forms.Button showDateButton;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.Label classLabel;
+        private System.Windows.Forms.RadioButton freshmanButton;
+        private System.Windows.Forms.RadioButton sophomoreButton;
+        private System.Windows.Forms.RadioButton juniorButton;
+        private System.Windows.Forms.RadioButton seniorButton;
+    }
+}
+
---
+++ Program 3/Form1.cs
@@ -0,0 +1,102 @@
+﻿// Program 3
+// CIS 199-75
+// Due: 4/11/2017
+// Grading ID: C3527
+// This program takes a user inputted last name, and based on their class standing
+// will output the date and time that the student may registration for classes. 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void showDateButton_Click(object sender, EventArgs e)
+        {
+            const string TIME1 = "8:30";                            // Constant to hold first time
+            const string TIME2 = "10:00";                           // Constant to hold second time
+            const string TIME3 = "11:30";                           // Constant to hold third time
+            const string TIME4 = "2:00";                            // Constant to hold fourth time
+            const string TIME5 = "4:00";                            // Constant to hold fifth time
+
+            const string SENIOR_DATE = "Wednesday, March 29th";     // Constant to hold the first date for seniors
+            const string JUNIOR_DATE = "Thursday, March 30th";      // Constant to hold the first date for juniors
+            const string SOPHOMORE_DATE_1 = "Friday, March 31st";   // Constant to hold the first possible date for sophomores
+            const string SOPHOMORE_DATE_2 = "Monday, April 3rd";    // Constant to hold the second possible date for sophomores
+            const string FRESHMAN_DATE_1 = "Tuesday, April 4th";    // Constant to hold the first possible date for freshman
+            const string FRESHMAN_DATE_2 = "Wednesday, April 5th";  // Constant to hold the second possible date for freshman
+
+            string lastName = lastNameTextBox.Text;             // Variable to hold the last name
+            char lastNameLetter = char.ToUpper(lastName[0]);    // Variable to hold the first letter of the last name and changes it to lowercase
+            bool isUpperClass;                                  // Variable to hold whether student is upper class
+
+            if (char.IsLetter(lastNameLetter)) // Tests if the lastNameLetter variable is a letter
+            {
+                isUpperClass = (seniorButton.Checked || juniorButton.Checked);
+
+                if (isUpperClass)
+                {
+                    if (seniorButton.Checked)
+                        dateOutputLabel.Text = SENIOR_DATE;
+                    else
+                        dateOutputLabel.Text = JUNIOR_DATE;
+
+                    char[] lastNameLowLimit = { 'A', 'E', 'J', 'P', 'T' };
+                    string[] times = { TIME3, TIME4, TIME5, TIME1, TIME2 };
+                    bool found = false;
+
+                    int index = lastNameLowLimit.Length - 1; 
+
+                    while (index >= 0 && !found)
+                    {
+                        if (lastNameLetter >= lastNameLowLimit[index])
+                            found = true;
+                        else
+                            --index;
+                    }
+                    if (found)
+                        timeOutputLabel.Text = times[index]; 
+                }
+                else // Must be sophomore/freshman
+                {
+                    if(sophomoreButton.Checked)
+                    {
+                        if ((lastNameLetter >= 'C') &&
+                            (lastNameLetter <= 'O'))
+                            dateOutputLabel.Text = SOPHOMORE_DATE_2;
+                        else
+                            dateOutputLabel.Text = SOPHOMORE_DATE_1;    
+                    }
+                    else
+                    {
+                        if ((lastNameLetter >= 'C') &&
+                            (lastNameLetter <= 'O'))
+                            dateOutputLabel.Text = FRESHMAN_DATE_2;
+                        else
+                            dateOutputLabel.Text = FRESHMAN_DATE_1;
+                    }
+                
+
+                }
+
+            }
+             else
+            {
+                MessageBox.Show("Please enter your last name.");
+            }
+        }
+    }
+}
